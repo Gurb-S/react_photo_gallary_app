a2e99431ef32cec03f3e726c276074770331c04b
gurb <Organicmintz@gmail.com>
2022-05-11 17:29:12 -0700

    add not found code

---
+++ src/components/NotFound.js
@@ -0,0 +1,8 @@
+export function NotFound() {
+    return(
+        <li className="not-found">
+            <h3>No Results Found</h3>
+            <p>You're search did not return any results. Please try again.</p>
+        </li>
+    )
+}
