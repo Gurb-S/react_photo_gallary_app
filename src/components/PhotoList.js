fca5ff8be845b4d17c60e457eed490dd2bdca590
gurb <Organicmintz@gmail.com>
2022-05-11 17:23:56 -0700

    add template imgs

---
+++ src/components/PhotoList.js
@@ -0,0 +1,10 @@
+import { Photo } from "./Photo";
+
+export function PhotoList() {
+    return(
+        <div className="photo-container">
+            <h2>Results</h2>
+            <Photo />
+        </div>
+    )
+}
