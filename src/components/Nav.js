3031bc2cca693023d200194a238f538012da0b67
gurb <Organicmintz@gmail.com>
2022-05-13 00:06:58 -0700

    add functionality to nav btns

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,18 @@
 import { NavLink } from "react-router-dom"
 
-export function Nav() {
+export function Nav(props) {
+
+    const handleClick = (e) =>{
+        console.log(e.target.textContent)
+        props.whenClicked(e.target.textContent)
+    }
     return(
         <nav className="main-nav">
             <ul>
-                <li><NavLink to="/cats">Cats</NavLink></li>
-                <li><NavLink to="/dogs">Dogs</NavLink></li>
-                <li><NavLink to="/computers">Computers</NavLink></li>
+                <li><NavLink to="/cats" onClick={handleClick}>Cats</NavLink></li>
+                <li><NavLink to="/dogs" onClick={handleClick}>Dogs</NavLink></li>
+                <li><NavLink to="/computers" onClick={handleClick}>Computers</NavLink></li>
             </ul>
-        </nav>
+        </nav> 
     )
 } 
