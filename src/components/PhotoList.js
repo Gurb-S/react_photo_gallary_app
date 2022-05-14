b4098163efd85612cf3655dba4ae0089c9371728
gurb <Organicmintz@gmail.com>
2022-05-13 21:24:55 -0700

    add key for photos

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,20 +1,21 @@
 import { useParams } from "react-router-dom";
+import { DisplayImgs } from "./DisplayImgs";
+import { NoResults } from "./NoResults";
 import { Photo } from "./Photo";
 
 export function PhotoList(props) {
-
+    
     const { search } = useParams();    
-
+    let key = 0;
     const photos = props.pics[0].map((pic) => {
-        return <Photo url={ pic } />
+        key++;
+        return <Photo url={ pic } key={key}/>
     });
 
     return(
-        <div className="photo-container">
-            <h2>{search}</h2>
-            <ul>
-                { photos }
-            </ul>
-        </div>
+        (props.pics[0].length === 0)
+        ? <NoResults />
+        :
+        <DisplayImgs photos={photos} search={search}/>
     )
 }
