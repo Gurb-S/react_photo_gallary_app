a8f788cb193f2848986d0781414c872893b02e69
gurb <Organicmintz@gmail.com>
2022-05-13 20:22:45 -0700

    add dynamic title

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,18 +1,17 @@
-
+import { useParams } from "react-router-dom";
 import { Photo } from "./Photo";
 
 export function PhotoList(props) {
-    // console.log(props.pics);
-    // console.log(props.pics[0])
 
+    const { search } = useParams();    
 
-    
     const photos = props.pics[0].map((pic) => {
         return <Photo url={ pic } />
     });
+
     return(
         <div className="photo-container">
-            <h2>Results</h2>
+            <h2>{search}</h2>
             <ul>
                 { photos }
             </ul>
