c69c7aab393de19cc0db187950c5aae2d4a49483
gurb <Organicmintz@gmail.com>
2022-05-13 21:24:21 -0700

    add no result page

---
+++ src/components/NoResults.js
@@ -0,0 +1,9 @@
+
+export function NoResults() {
+    return(
+        <div>
+            <img src={require("../sad_face.png")} height={200} width={200} alt="sad_face"/>
+            <h2>Sorry no images found</h2>
+        </div>
+    )
+}
