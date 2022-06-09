82eb5888e8e447c4045ae4a3ba430e3b2403a045
gurb <Organicmintz@gmail.com>
2022-06-09 14:34:40 -0700

    allow browser front and back btns to search

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,60 +1,39 @@
-//import { useEffect } from "react";
-import { useNavigate, useParams} from "react-router-dom";
+import { useEffect } from "react";
+import {  useParams} from "react-router-dom";
 import { DisplayImgs } from "./DisplayImgs";
 import { NoResults } from "./NoResults";
 import { Photo } from "./Photo";
+import { LoadingCircle } from "./LoadingCircle";
 
 export function PhotoList(props) {
 
     const { search } = useParams();  
-    const navigate = useNavigate();
+    //const navigate = useNavigate();
 
     console.log(search)  
+    useEffect(() => {
+        console.log('TESTINGGGG!!!!!!!!!!!!!')
+        props.onSearch(search)
+    }, [search])
     
-    //console.log(props.loading)
-    // if(props.loading){
-    //     console.log('REDIRECTING NOW.....')
-    //     navigate('/')
-    // }
-    //props.onUrlChange(search);
-    if(search === undefined){
-        console.log('BASIC UNDEFINED')
-    }
-    else if(search === 'undefined'){
-        console.log('STRING UNDEFINED')
-    }
-    else{
-        console.log("SEARCH DON'T MATTER BRUH")
-    }
-//jdhfdgfjhdsg
     console.log(props.pics)
     if(props.pics[0] === undefined){
-        console.log("THERE'S AN ISSUEEEEEE")
-        props.onSearch(search)
-        console.log(search);
-        console.log('TASK HAS BEEN EXECUTUED')
+        // console.log("THERE'S AN ISSUEEEEEE")
+        // // props.onSearch(search)
+        // console.log(search);
+        // console.log('TASK HAS BEEN EXECUTUED')
         return;
         //props.onUrlChange(search)
-        
     }
-    // useEffect(() =>{
-    //     if(props.pics === undefined){
-    //         navigate('/')
-    //         console.log('I DID IT!!!!!!!!!!!!!!!!!!!!!!!')
-    //     }
-        
-    // }, [])
     let key = 0;
     const photos = props.pics[0].map((pic) => {
         key++;
         return <Photo url={ pic } key={key}/>
     });
-    console.log(props.pics[0].length);
+//     console.log(props.pics[0].length);
+
 
     return(
-        (props.pics[0].length === 0)
-        ? <NoResults />
-        : <DisplayImgs photos={photos} search={search}/>
-        
+        (props.loading ? <LoadingCircle /> : (props.pics[0].length === 0) ? <NoResults /> : <DisplayImgs photos={photos} search={search}/>)
     )
 }
